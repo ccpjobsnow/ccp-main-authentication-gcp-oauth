94aada34500916f4dbcfda4750b6bc22dda0bd5a
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-20 19:41:36 -0300

    .

--- src/main/java/com/ccp/implementations/main/authentication/gcp/oauth/CcpGcpMainAuthentication.java
+++ src/main/java/com/ccp/implementations/main/authentication/gcp/oauth/CcpGcpMainAuthentication.java
@@ -1,10 +1,11 @@
 package com.ccp.implementations.main.authentication.gcp.oauth;
 
 import com.ccp.dependency.injection.CcpInstanceProvider;
+import com.ccp.especifications.main.authentication.CcpAuthenticationProvider;
 
-public class CcpGcpMainAuthentication implements CcpInstanceProvider {
+public class CcpGcpMainAuthentication implements CcpInstanceProvider<CcpAuthenticationProvider> {
 
-	public Object getInstance() {
+	public CcpAuthenticationProvider getInstance() {
 		return new GcpOauthAuthenticationProvider();
 	}
 
